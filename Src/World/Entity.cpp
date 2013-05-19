f644948e3750f0e70c5bdf464fb5f515b14dea53
Dan Watkins <bluehailex@gmail.com>
2013-05-19 12:53:27 -0500

    Forgot to clean project...

---
+++ Src/World/Entity.cpp
@@ -0,0 +1,24 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+	mId = -1;
+	mEntityType = EntityType::Default;
+	mTagged = false;
+	mBoundingRadius = 2.0;
+}
+
+
+Entity::Entity(EntityType entityType)
+{
+	Entity::Entity();
+	entityType = entityType;
+}
+
+
+Entity::Entity(EntityType entityType, Vector2D<double> pos, double radius)
+{
+	Entity::Entity(entityType);
+	SetPos(pos);
+	SetBoundingRadius(radius);
+}
