3e66d1b186a68489c0857d2d9bbcdef1dd422960
Dan Watkins <bluehailex@gmail.com>
2013-05-29 20:06:19 -0500

    Added the rest of the private methods for SteeringBehaviors

--- Src/World/SteeringBehavior.cpp
+++ Src/World/SteeringBehavior.cpp
@@ -2,4 +2,103 @@
 
 namespace ait
 {
+	bool SteeringBehavior::AccumulateForce(Vector2D<double> &sf, Vector2D<double> forceToAdd)
+	{
+
+	}
+
+	void SteeringBehavior::CreateFeelers()
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Seek(Vector2D<double> targetPos)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Flee(Vector2D<double> targetPos)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Arrive(Vector2D<double> targetPos, Deceleration deceleration)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Pursuit(const Vehicle *agent)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::OffsetPursuit(const Vehicle *agent, const Vector2D<double> offset)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Evade(const Vehicle *agent)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Wander()
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::ObstacleAvoidance(const std::vector<Entity*> &obstacles)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::WallAvoidance(const std::vector<Wall2D> &walls)
+	{
+	}
+
+
+
+	Vector2D<double> SteeringBehavior::FollowPath()
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Interpose(const Vehicle *vehicleA, const Vehicle *vehicleB)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Hide(const Vehicle *hunter, const std::vector<Entity*> &obstacles)
+	{
+	}
+	
+
+	Vector2D<double> SteeringBehavior::Cohesion(const std::vector<Vehicle*> &agents)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Separation(const std::vector<Vehicle*> &agents)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::Alignment(const std::vector<Vehicle*> &agents)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::CohesionPlus(const std::vector<Vehicle*> &agents)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::SeparationPlus(const std::vector<Vehicle*> &agents)
+	{
+	}
+
+
+	Vector2D<double> SteeringBehavior::AlignmentPlus(const std::vector<Vehicle*> &agents)
+	{
+	}
 };
--- Src/World/SteeringBehavior.h
+++ Src/World/SteeringBehavior.h
@@ -5,38 +5,51 @@
 
 namespace ait
 {
-	enum SummingMethod
-	{
-		WeightedAverage,
-		Prioritized,
-		Dithered
-	};
+	//the radius of the constraining circle for the wander behavior
+	const double gWanderRadius = 1.2;
 
+	//the distance the wander circle is projected in front of the angent
+	const double gWanderDistance = 2.0;
 
-	enum BehaviorType
-	{
-		None				= 0x00000,
-		Seek				= 0x00002,
-		Flee				= 0x00004,
-		Arrive				= 0x00008,
-		Wander				= 0x00010,
-		Cohesion			= 0x00020,
-		Separation			= 0x00040,
-		Allignment			= 0x00080,
-		ObstacleAvoidance	= 0x00100,
-		WallAvoidance		= 0x00200,
-		FollowPath			= 0x00400,
-		Pursuit				= 0x00800,
-		Evade				= 0x01000,
-		Interpose			= 0x02000,
-		Hide				= 0x04000,
-		Flock				= 0x08000,
-		OffsetPursuit		= 0x10000
-	};
+	//the maximium amount of displacement along the circle each frame
+	const double gWanderJitterPerSec = 80.0;
+
+	//used in path following
+	const double gWaypointSeekDistance = 20.0;
 
 
 	class SteeringBehavior
 	{
+	public:
+		enum SummingMethod
+		{
+			WeightedAverage,
+			Prioritized,
+			Dithered
+		};
+
+	private:
+		enum BehaviorType
+		{
+			None				= 0x00000,
+			Seek				= 0x00002,
+			Flee				= 0x00004,
+			Arrive				= 0x00008,
+			Wander				= 0x00010,
+			Cohesion			= 0x00020,
+			Separation			= 0x00040,
+			Allignment			= 0x00080,
+			ObstacleAvoidance	= 0x00100,
+			WallAvoidance		= 0x00200,
+			FollowPath			= 0x00400,
+			Pursuit				= 0x00800,
+			Evade				= 0x01000,
+			Interpose			= 0x02000,
+			Hide				= 0x04000,
+			Flock				= 0x08000,
+			OffsetPursuit		= 0x10000
+		};
+
 		Vehicle *mVehicle;
 		Vehicle *mTargetAgent1;
 		Vehicle *mTargetAgent2;
@@ -71,7 +84,46 @@ namespace ait
 		double mWeightFollowPath;
 
 		double mViewDistance;
-		 //Path *mPath;
+
+		Path *mPath;
+
+		double mWaypointSeekDistanceSq;
+		Vector2D<double> mOffset;
+		int mFlags;
+
+		enum Deceleration { Slow = 3, Normal = 2, Fast = 1 };
+		Deceleration mDeceleration;
+
+		bool mCellSpaceOn;
+		SummingMethod mSummingMethod;
+
+		//methods
+		bool On(BehaviorType behaviorType) { return (mFlags & behaviorType) == behaviorType; }
+		bool AccumulateForce(Vector2D<double> &sf, Vector2D<double> forceToAdd);
+		void CreateFeelers();
+
+
+		//behavior methods
+		Vector2D<double> Seek(Vector2D<double> targetPos);
+		Vector2D<double> Flee(Vector2D<double> targetPos);
+		Vector2D<double> Arrive(Vector2D<double> targetPos, Deceleration deceleration);
+		Vector2D<double> Pursuit(const Vehicle *agent);
+		Vector2D<double> OffsetPursuit(const Vehicle *agent, const Vector2D<double> offset);
+		Vector2D<double> Evade(const Vehicle *agent);
+		Vector2D<double> Wander();
+		Vector2D<double> ObstacleAvoidance(const std::vector<Entity*> &obstacles);
+		Vector2D<double> WallAvoidance(const std::vector<Wall2D> &walls);
+		Vector2D<double> FollowPath();
+		Vector2D<double> Interpose(const Vehicle *vehicleA, const Vehicle *vehicleB);
+		Vector2D<double> Hide(const Vehicle *hunter, const std::vector<Entity*> &obstacles);
+
+		Vector2D<double> Cohesion(const std::vector<Vehicle*> &agents);
+		Vector2D<double> Separation(const std::vector<Vehicle*> &agents);
+		Vector2D<double> Alignment(const std::vector<Vehicle*> &agents);
+
+		Vector2D<double> CohesionPlus(const std::vector<Vehicle*> &agents);
+		Vector2D<double> SeparationPlus(const std::vector<Vehicle*> &agents);
+		Vector2D<double> AlignmentPlus(const std::vector<Vehicle*> &agents);
 	};
 };
 
